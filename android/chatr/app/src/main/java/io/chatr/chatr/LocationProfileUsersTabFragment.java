0f37b75f41921d0870220198d5d736233dfe219d
Daniel Briskin <daniel.briskin@gmail.com>
2018-02-27 23:57:54 -0500

    Tabbed layout in collapsing toolbar layout with ability to swipe between tabs

---
+++ android/chatr/app/src/main/java/io/chatr/chatr/LocationProfileUsersTabFragment.java
@@ -0,0 +1,22 @@
+package io.chatr.chatr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 2018-02-27.
+ */
+
+public class LocationProfileUsersTabFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_location_profile_users_tab, container, false);
+//        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+}
