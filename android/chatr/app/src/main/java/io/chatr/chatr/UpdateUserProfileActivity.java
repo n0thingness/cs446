fe676263fda797c9bab6af30de0a04044cf72589
Lohit Talasila <ltalasila@uwaterloo.ca>
2018-03-24 09:58:23 -0400

    added interestes text field, yet to connect

--- android/chatr/app/src/main/java/io/chatr/chatr/UpdateUserProfileActivity.java
+++ android/chatr/app/src/main/java/io/chatr/chatr/UpdateUserProfileActivity.java
@@ -13,8 +13,8 @@ public class UpdateUserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
