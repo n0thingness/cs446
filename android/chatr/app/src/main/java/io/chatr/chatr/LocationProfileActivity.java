9a3b789f5dcd84225c5e5a729e9bca2c54ebceb7
Daniel Briskin <daniel.briskin@gmail.com>
2018-03-25 01:09:34 -0400

    Async task loader setup

--- android/chatr/app/src/main/java/io/chatr/chatr/LocationProfileActivity.java
+++ android/chatr/app/src/main/java/io/chatr/chatr/LocationProfileActivity.java
@@ -1,10 +1,14 @@
 package io.chatr.chatr;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +20,9 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-public class LocationProfileActivity extends AppCompatActivity {
+import io.chatr.chatr.data.model.Location;
+
+public class LocationProfileActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Location> {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -32,6 +38,8 @@ public class LocationProfileActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+
+    int query_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +77,12 @@ public class LocationProfileActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        Bundle b = getIntent().getExtras();
+        query_id = b.getInt("id");
+
+        getSupportLoaderManager().initLoader(0, null, (LoaderManager.LoaderCallbacks<Location>)this).forceLoad();
+
+
         //Location image URL
         String url = "http://www.simcoedining.com/img/venue_photos/williams-cafe-barrie.jpg";
         Glide.with(this).load(url).into(top_image);
@@ -96,6 +110,78 @@ public class LocationProfileActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public Loader<Location> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Location> loader, Location data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Location> loader) {
+
+    }
+
+
+
+
+    private static class FetchData extends AsyncTaskLoader<Location> {
+
+        public FetchData(Context context) {
+            super(context);
+        }
+
+        @Override
+        public Location loadInBackground() {
+//            HttpURLConnection urlConnection = null;
+//            BufferedReader reader = null;
+//            String jsonStr = null;
+//            String line;
+//            try {
+//                URL url = new URL("https://itunes.apple.com/search?term=classic");
+//                urlConnection = (HttpURLConnection) url.openConnection();
+//                urlConnection.setRequestMethod("GET");
+//                urlConnection.connect();
+//
+//                // Read the input stream into a String
+//                InputStream inputStream = urlConnection.getInputStream();
+//                StringBuffer buffer = new StringBuffer();
+//                if (inputStream == null) return null;
+//
+//                reader = new BufferedReader(new InputStreamReader(inputStream));
+//                while ((line = reader.readLine()) != null) buffer.append(line);
+//
+//                if (buffer.length() == 0) return null;
+//                jsonStr = buffer.toString();
+//
+//            } catch (IOException e) {
+//                Log.e("MainActivity", "Error ", e);
+//                return null;
+//            } finally {
+//                if (urlConnection != null) urlConnection.disconnect();
+//                if (reader != null) {
+//                    try {
+//                        reader.close();
+//                    } catch (final IOException e) {
+//                        Log.e("MainActivity", "Error closing stream", e);
+//                    }
+//                }
+//            }
+
+            return new Location();
+        }
+
+        @Override
+        public void deliverResult(Location data) {
+            super.deliverResult(data);
+        }
+    }
+
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
---
+++ android/chatr/app/src/main/java/io/chatr/chatr/data/model/Location.java
@@ -0,0 +1,24 @@
+package io.chatr.chatr.data.model;
+
+/**
+ * Created by Daniel on 2018-03-25.
+ */
+
+public class Location {
+    private int id;
+    private int google_id;
+    private String name;
+    private String address;
+    private int priceLevel;
+    private int rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
